f05a2803f2e44cddb50489da56f7675cd5b5ae5f
yaxnn <yaseenharish@gmail.com>
2024-10-28 02:07:04 +0530

    right side nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { HiMiniBars3CenterLeft } from "react-icons/hi2";
 import { IoSearchOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi";
+import { HiOutlineHeart } from "react-icons/hi";
+import { HiOutlineShoppingCart } from "react-icons/hi";
+import avatarImg from "../assets/avatar.png"
 
 const Navbar = () => {
+
+    const currentUser = true;
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
       <nav className="flex justify-between items-center">
@@ -15,7 +21,7 @@ const Navbar = () => {
 
           {/* {search input} */}
           <div className="relative sm:w-72 w-40 space-x-2">
-            <IoSearchOutline  className="absolute inline-block left-3 inset-y-2"/>
+            <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
             <input
               type="text"
               placeholder="search here"
@@ -25,7 +31,25 @@ const Navbar = () => {
         </div>
 
         {/* {right side} */}
-        <div>nav items</div>
+        <div className="relative flex items-center md:space-x-3 space-x-2">
+            <div>
+                {
+                currentUser ? <>
+                <button>
+                    <img src={avatarImg} alt="" className={`size-7 rounded-full ${currentUser ? 'ring-2 ring-blue-500' : ''}`} />
+                </button>
+                </> : <Link to="/login"><HiOutlineUser className="size-6" /></Link>
+                }
+            </div>
+          <button className="hidden sm:block">
+            <HiOutlineHeart className="size-6" />
+          </button>
+
+          <Link to="/cart" className="bg-primary p-1 sm:px-6 px-2 flex items-center">
+            <HiOutlineShoppingCart className="size-6" />
+            <span className="text-sm font-semibold sm:ml-1">0</span>
+          </Link>
+        </div>
       </nav>
     </header>
   );
