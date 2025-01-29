da6b8508948c61338ebf981cfb88f50150d3e33d
yaxnn <yaseenharish@gmail.com>
2025-01-30 00:20:29 +0530

    get all

--- backend/src/games/game.controller.js
+++ backend/src/games/game.controller.js
@@ -1,14 +1,55 @@
+const Game = require("./game.model");
+
 const postAGame = async (req,res) => {
     try {
         const newGame = await Game({...req.body});
         await newGame.save();
-        res.status(200).send({message: "Game posted successfully", game: newGame})
+        res.status(200).send({message: "Game Posted Successfully", game: newGame})
     } catch (error) {
-        console.error("Error creating game",error);
-        res.status(400).send({message: "Failed to create game"})
+        console.error("Error Creating Game",error);
+        res.status(400).send({message: "Failed To Create Game"})
     }
 }
 
+// get all games
+
+const getAllGames = async (req,res) => {
+    try {
+        const games = await Game.find().sort({ createdAt: -1})
+        res.status(200).send(games)
+
+    } catch (error) {
+        console.error("Error Fetching Games",error);
+        res.status(400).send({message: "Failed To Fetch Games"})
+    }
+
+}
+
+// get single game
+const getSingleGame = async (req,res) =>{
+    try {
+        const {id} = req.params;
+        const game = await Game.findById(id);
+        if (!game) {
+            res.status(404).send({message: "Game Not Found!"})
+        }
+        res.status(200).send(game)
+
+    } catch (error) {
+        console.error("Error Fetching Game",error);
+        res.status(400).send({message: "Failed To Fetch Game"})
+    }
+}
+
+
+
+
+
+
+
+
 module.exports = {
-    postAGame
+    postAGame,
+    getAllGames,
+    getSingleGame
 }
--- backend/src/games/game.route.js
+++ backend/src/games/game.route.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router();
 const Game = require('./game.model')
-const {postAGame} = require('./game.controller');
+const {postAGame, getAllGames, getSingleGame} = require('./game.controller');
 
 // post a game 
 router.post("/create-game", postAGame)
+
+// get game
+router.get("/",getAllGames )
+
+// single game
+router.get("/:id",getSingleGame )
+
 
 
 module.exports = router;
