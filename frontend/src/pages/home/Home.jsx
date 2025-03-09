967a6aebdf2c24e083f1e832f545b4005cc4e6a9
yaxnn <yaseenharish@gmail.com>
2025-03-09 16:26:56 +0530

    new

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Banner from './Banner'
-import TopSellers from './TopSellers'
-import Recommended from './Recommended'
-import News from './News'
+import Banner from './Banner.jsx'
+import TopSellers from './TopSellers.jsx'
+import Recommended from './Recommended.jsx'
+import News from './News.jsx'
 
 
 const Home = () => {
